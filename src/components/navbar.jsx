db4daa450cf377e94ff9b400104a41319bc0555e
syncat <mourrane2000@gmail.com>
2023-11-04 06:49:29 +0100

    first commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,9 +8,13 @@ const Navbar = () => {
                 <h1>Syncat</h1>
             </div>
             <div className="search">
-                <input type="button" value="All Categories"/>
-                <span>|</span>
+                <select>
+                    <option value="computers">Computers</option>
+                    <option value="electronics">Electronics</option>
+                    <option value="clothes">Clothes</option>
+                </select>
                 <input type="text" value="" placeholder="Search for a product"/>
+                <img src="/images/search.png" width={25}/>
             </div>
             <div className="right-side">
                 <img src='/images/cart.png' width={50}/>
